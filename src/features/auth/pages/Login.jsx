167503a6c65bf7b85afd42ec4b742a265964b939
sujit_kumar <sujitkm9198@gmail.com>
2026-07-25 12:47:05 +0530

    login and register user validation updated

--- src/features/auth/pages/Login.jsx
+++ src/features/auth/pages/Login.jsx
@@ -9,66 +9,57 @@ const Login = () => {
   const [password, setPassword] = useState("")
   const [message, setMessage] = useState("");
   const [type, setType] = useState("");
-  const [errorMessage,setErrorMessage]=useState("")
+  const [errorMessage, setErrorMessage] = useState("")
 
   const { loginWithEmailPassword } = useFireBase()
-  const navigate=useNavigate();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    if (message) {
+      const timer = setTimeout(() => {
+        setMessage("");
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [message]);
 
   const handleSubmit = async (e) => {
-    // alert('called')
     e.preventDefault();
     const userEmail = email.trim();
 
     // Validation
     if (!userEmail || !password) {
-        setErrorMessage("Please fill all fields.");
+      setMessage("Please fill all fields.");
+      setType("danger")
       return;
     }
 
     if (password.length < 8) {
-        setErrorMessage("Password should be at least 8 characters.");
+      setMessage("Password should be at least 8 characters.");
+      setType("danger")
       return;
     }
 
     try {
       const result = await loginWithEmailPassword(email, password);
-
-      alert("User Login successfully!");
-      localStorage.setItem("loggedInUser",true)
+      setType("info")
+      setMessage("User Login successfully!");
+      localStorage.setItem("loggedInUser", true)
       setEmail("");
       setPassword("");
       setErrorMessage("")
       navigate("/")
     } catch (error) {
-      console.log("wwwwwwwwwwwwwwwww",error)
-
-      switch (error.code) {
-        case "auth/email-already-in-use":
-            setErrorMessage("Email already exists.");
-          break;
-
-        case "auth/invalid-email":
-            setErrorMessage("Invalid email address.");
-          break;
-
-        case "auth/weak-password":
-            setErrorMessage("Password is too weak.");
-          break;
-
-        case "auth/invalid-credential":
-            setErrorMessage("User does not Exist!");
-          break;
-
-        default:
-            setErrorMessage(error.message);
-      }
+      setType("danger");
+      const errorCode =error?.response?.data?.error?.message || error?.message;  
+      setMessage("Invalid Credentials");
     }
   };
 
   const customCss = {
     marginLeft: '38%', maxWidth: '80%', fontSize: '12px', fontFamily: 'Segoe UI', color: '#f5222d', textAlign: 'left'
-}
+  }
 
   return (
     <>
@@ -76,31 +67,40 @@ const Login = () => {
         <div className="card shadow p-4" style={{ width: "400px" }}>
           <h2 className="text-center mb-4">Login</h2>
 
-          {message && (
+          {/* {message && (
+            <Alert variant={type}>
+              {message}
+            </Alert>
+          )} */}
+            {message && (
             <div className={`alert alert-${type}`} role="alert">
               {message}
             </div>
           )}
 
-                  <form>
-                      <div className="mb-3">
-                          <label for="exampleInputEmail1" className="form-label">Email address</label>
-                          <input type="email" className="form-control" placeholder="enter your email" aria-describedby="emailHelp" value={email}
-                              onChange={(e) => setEmail(e.target.value)} id="email"
-                              autoComplete="off" />
-                          {/* {errorMessage && <h6 className='customCss' style={customCss}>{errorMessage}</h6>} */}
-                      </div>
+          <form>
+            <div className="mb-3">
+              <label for="exampleInputEmail1" className="form-label">Email</label>
+              <input type="email" className="form-control" placeholder="enter your email" aria-describedby="emailHelp" value={email}
+                onChange={(e) => setEmail(e.target.value)} id="email"
+                autoComplete="off" />
+            </div>
 
 
-                      <div className="mb-3">
-                          <label for="exampleInputPassword1" className="form-label">Password</label>
-                          <input type="password" className="form-control" value={password}
-                              onChange={(e) => setPassword(e.target.value)} id="password"
-                              autoComplete="off" />
-                          {/* {errorMessage && <h6 className='customCss' style={customCss}>{errorMessage}</h6>} */}
-                      </div>
-                      <Button variant="success" onClick={handleSubmit}>Login</Button>
-                  </form>
+            <div className="mb-3">
+              <label for="exampleInputPassword1" className="form-label">Password</label>
+              <input type="password" className="form-control" value={password} placeholder="enter your password"
+                onChange={(e) => setPassword(e.target.value)} id="password"
+                autoComplete="off" />
+            </div>
+            <Button
+              variant="success"
+              className="w-100"
+              onClick={handleSubmit}
+            >
+              Login
+            </Button>
+          </form>
 
           <div className="text-center mt-3">
             <small>
--- src/features/auth/pages/RegisterUser.jsx
+++ src/features/auth/pages/RegisterUser.jsx
@@ -12,14 +12,22 @@ const RegisterUser = () => {
   const [confirmPassword, setconfirmPassword] = useState("")
   const [message, setMessage] = useState("");
   const [type, setType] = useState("");
-  const [errorMessage,setErrorMessage]=useState("")
+  const [errorMessage, setErrorMessage] = useState("")
 
 
-  const { createUserUsingEmailPassword, updateUserProfile, saveUserToFirestore } = useFireBase()
+  const { createUserUsingEmailPassword, updateUserProfile, saveUserToFirestore } = useFireBase();
+
+  useEffect(() => {
+    if (message) {
+      const timer = setTimeout(() => {
+        setMessage("");
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [message]);
 
   const handleSubmit = async (e) => {
-    debugger
-    // e.preventDefault();
     // Trim values
     const firstName = fName.trim();
     const lName = lastName.trim();
@@ -27,17 +35,22 @@ const RegisterUser = () => {
 
     // Validation
     if (!firstName || !lName || !userEmail || !password || !confirmPassword) {
-      Alert("Please fill all fields.");
+      setMessage("Please fill all fields.");
+      setType("danger");
       return;
     }
 
     if (password !== confirmPassword) {
-      setErrorMessage("Passwords do not match.");
+      setMessage("Passwords do not match.");
+      setType("danger");
+
       return;
     }
 
     if (password.length < 8) {
-      setErrorMessage("Password should be at least 8 characters.");
+      setMessage("Password should be at least 8 characters.");
+      setType("danger");
+
       return;
     }
 
@@ -48,41 +61,44 @@ const RegisterUser = () => {
 
       await saveUserToFirestore(result.user, fName, lastName);
 
-      Alert("User created successfully!");
+      setMessage("User created successfully!");
 
       setFName("");
       setlastName("");
       setEmail("");
       setPassword("");
       setconfirmPassword("");
-    } catch (error) {
-      switch (error.code) {
-        case "auth/email-already-in-use":
-          setErrorMessage("Email already exists.");
+    }
+    catch (error) {
+      setType("danger");
+
+      const errorCode = error?.response?.data?.error?.message || error?.message;
+      if(errorCode==='EMAIL_EXISTS'){
+          setMessage("Email already exists.");
+      }
+
+      switch (errorCode==='EMAIL_EXISTS') {
+        case "EMAIL_EXISTS":
           break;
 
-        case "auth/invalid-email":
-          setErrorMessage("Invalid email address.");
+        case "INVALID_EMAIL":
+          setMessage("Invalid email address.");
           break;
 
-        case "auth/weak-password":
-          setErrorMessage("Password is too weak.");
+        case "WEAK_PASSWORD":
+          setMessage("Password is too weak.");
           break;
 
         default:
-          setErrorMessage(error.message);
+          setMessage(errorCode || "Something went wrong.");
       }
     }
   };
 
-  const customCss = {
-    marginLeft: '38%', maxWidth: '80%', fontSize: '12px', fontFamily: 'Segoe UI', color: '#f5222d', textAlign: 'left'
-}
-
   return (
     <>
       <div className="container vh-100 d-flex justify-content-center align-items-center">
-        <div className="card shadow p-4" style={{ width: "400px" }}>
+        <div className="card shadow p-4" style={{ width: "572px" }}>
           <h2 className="text-center mb-4">SignUp Here</h2>
 
           {message && (
@@ -93,57 +109,95 @@ const RegisterUser = () => {
 
           <form>
             <div className="mb-3">
-              <Button>Sign In with Google</Button>
+              <Button variant="success" className="w-100">
+                Sign In with Google
+              </Button>
             </div>
 
-            <div className="mb-3">
-              <label for="firstName" className="form-label">First Name</label>
-              <input type="text" className="form-control" placeholder="enter your first name" aria-describedby="fnameHelp" value={fName}
-                onChange={(e) => setFName(e.target.value)} id="fName"
-                autoComplete="off" />
-                {errorMessage && <h6 className='customCss' style={customCss}>{errorMessage}</h6>}
-
+            <div className="row mb-3 align-items-center">
+              <label htmlFor="fName" className="col-sm-3 col-form-label">
+                First Name
+              </label>
+              <div className="col-sm-9">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="fName"
+                  placeholder="Enter your first name"
+                  value={fName}
+                  onChange={(e) => setFName(e.target.value)}
+                />
+              </div>
             </div>
 
-            <div className="mb-3">
-              <label for="lastName" className="form-label">Last Name</label>
-              <input type="text" className="form-control" placeholder="enter your last name" aria-describedby="lnameHelp" value={lastName}
-                onChange={(e) => setlastName(e.target.value)} id="lName"
-                autoComplete="off" />
-                          {errorMessage && <h6 className='customCss' style={customCss}>{errorMessage}</h6>}
-
+            <div className="row mb-3 align-items-center">
+              <label htmlFor="lName" className="col-sm-3 col-form-label">
+                Last Name
+              </label>
+              <div className="col-sm-9">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="lName"
+                  placeholder="Enter your last name"
+                  value={lastName}
+                  onChange={(e) => setlastName(e.target.value)}
+                />
+              </div>
             </div>
 
-            <div className="mb-3">
-              <label for="exampleInputEmail1" className="form-label">Email address</label>
-              <input type="email" className="form-control" placeholder="enter your email" aria-describedby="emailHelp" value={email}
-                onChange={(e) => setEmail(e.target.value)} id="email"
-                autoComplete="off" />
-                          {errorMessage && <h6 className='customCss' style={customCss}>{errorMessage}</h6>}
+            <div className="row mb-3 align-items-center">
+              <label htmlFor="email" className="col-sm-3 col-form-label">
+                Email
+              </label>
+              <div className="col-sm-9">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
 
+            <div className="row mb-3 align-items-center">
+              <label htmlFor="password" className="col-sm-3 col-form-label">
+                Password
+              </label>
+              <div className="col-sm-9">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="row mb-3 align-items-center">
+              <label htmlFor="confirmPass" className="col-sm-3 col-form-label">
+                Confirm Password
+              </label>
+              <div className="col-sm-9">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="confirmPass"
+                  placeholder="Confirm your password"
+                  value={confirmPassword}
+                  onChange={(e) => setconfirmPassword(e.target.value)}
+                />
+              </div>
             </div>
 
 
-            <div className="mb-3">
-              <label for="exampleInputPassword1" className="form-label">Password</label>
-              <input type="password" className="form-control" value={password}
-                onChange={(e) => setPassword(e.target.value)} id="password"
-                autoComplete="off" />
-                          {errorMessage && <h6 className='customCss' style={customCss}>{errorMessage}</h6>}
-
-            </div>
-
-            <div className="mb-3">
-              <label for="confirmPass" className="form-label">Confirm Password</label>
-              <input type="password" className="form-control" value={confirmPassword}
-                onChange={(e) => setconfirmPassword(e.target.value)} id="confirmPass"
-                autoComplete="off" />
-                          {errorMessage && <h6 className='customCss' style={customCss}>{errorMessage}</h6>}
-
-            </div>
-
-            <Button variant="success" onClick={handleSubmit}>Register</Button>
-
+            <Button variant="success" className="w-100" onClick={handleSubmit}>
+              Register
+            </Button>
           </form>
 
           <div className="text-center mt-3">
